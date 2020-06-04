fff210e366de70dc70892f264e6b1145fa406d46
danocolombo@google.com <GR0mans1212!>
2020-06-04 08:24:08 -0400

    progressing client privledges

--- routes/api/client.js
+++ routes/api/client.js
@@ -70,14 +70,25 @@ router.get('/code/:code', auth, async (req, res) => {
 // @access   Private
 router.get('/privledges/:code/:uid', auth, async (req, res) => {
     try {
-        // console.log('API::cid: ' + req.param.cid);
-        // const client = await Client.findOne({ code: req.params.code });
-        const client = null;
-        if (!client) {
+        //----------------------------------------------
+        // get the client info
+        const cData = await Client.find({ code: req.params.code }).select(
+            '-_id'
+        );
+        if (Object.keys(cData).length === 0) {
+            return res.status(400).json({ msg: 'client not found' });
+        }
+
+        var p = JSON.parse(JSON.stringify(cData));
+        const church = p[0];
+        if (church.name) console.log('church: ' + church.name);
+
+        const clientPrivs = null;
+        if (!clientPrivs) {
             return res.status(400).json({ msg: 'Cannot process request' });
         }
 
-        res.json(client);
+        res.json(clientPrivs);
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error');
