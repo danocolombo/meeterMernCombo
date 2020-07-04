47574ce9e2743e4e6d30eeaaabf198ca9723edac
danocolombo <danocolombo@gmail.com>
2020-07-04 07:26:00 -0400

    formatting group list on editGathering

--- client/src/components/gatherings/GroupListItem.js
+++ client/src/components/gatherings/GroupListItem.js
@@ -12,36 +12,20 @@ const GroupListItem = ({
     group: { _id, gender, title, location, facilitator },
 }) => (
     <Fragment>
-        <div className={'GGL-Box'} id={_id}>
-            <div className='GGL-Gender my-1 pl-1 pr-1'>
+        <div className="GItem-Box">
+            
+            <div className={'GItem-GenderTitle'}>
                 <Link to={`/EditGroup/${mid}/${_id}`}>
-                    {gender === 'f' && <div>women</div>}
-                    {gender === 'm' && <div>men</div>}
-                    {gender === 'x' && <div>mixed</div>}
-                </Link>
+                    {gender === 'f' && <div>women's</div>}
+                    {gender === 'm' && <div>men's</div>}
+                </Link>{title}
             </div>
-
-            <div>
-                <p className='GGL-Title my-1 pr-1'>{title}</p>
-            </div>
-            <div>
-                <p className='GGL-Location my-1'>{location}</p>
-            </div>
-            <div>
-                <p className='GGL-Facilitator my-1 pl'>{facilitator}</p>
-            </div>
-
-            <div className='GGL-Button my'>
-                <button
-                    onClick={() => deleteGroup(_id, mid)}
-                    type='button'
-                    className='btn btn-danger my0'
-                >
-                    <i className='fas fa-times' />
-                </button>
-            </div>
+            <div className="GItem-LocationFacilitator">
+                <p>Location</p><p>Facilitator</p></div>
+            <div className="GItem-Button">Button</div>
         </div>
     </Fragment>
+    // <p style={{ 'padding-left': 10 }}>
 );
 
 GroupListItem.propTypes = {
